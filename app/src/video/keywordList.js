306b121df90b2628831f2c2b9272f9139b0d659a
uhartegr <remi.uh@gmail.com>
2024-01-25 11:29:13 +0100

    Add the keyword list

---
+++ app/src/video/keywordList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import data from '../data.json';
+
+const KeywordList = ({ currentPosition }) => {
+  const [currentKeywords, setCurrentKeywords] = useState([]);
+
+  useEffect(() => {
+    const updateKeywords = () => {
+      const keywords = data.Keywords.filter((keyword) => parseFloat(keyword.pos) <= currentPosition);
+      console.log("keywords:", keywords);
+      if (keywords.length > 0) {
+        setCurrentKeywords(keywords[keywords.length - 1].data);
+      } else {
+        setCurrentKeywords([]);
+      }
+    };
+    
+    updateKeywords(); 
+
+    return () => {
+    };
+  }, [currentPosition]);
+
+  return (
+    <div className="keyword-list">
+      <h3>Keywords</h3>
+      <ul>
+        {currentKeywords.map((keyword, index) => (
+          <li key={index}>
+            <a href={keyword.url}>
+              {keyword.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default KeywordList;
