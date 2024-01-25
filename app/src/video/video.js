6e143c850d5a969885194dfec055453b2997a774
uhartegr <remi.uh@gmail.com>
2024-01-25 11:29:44 +0100

    Add the keyword list and update ChapterList call

--- app/src/video/video.js
+++ app/src/video/video.js
@@ -1,34 +1,34 @@
-// video.js
-import React, { useRef } from 'react';
-import { Player } from 'video-react';
-
+// Video.js
+import React, { useRef, useState } from 'react';
+import { Player, ControlBar, PlayToggle, ProgressControl } from 'video-react';
 import ChapterList from './chapterList';
+import KeywordList from './keywordList';
 import data from '../data.json';
 
 import 'video-react/dist/video-react.css';
 
 const Video = () => {
   const playerRef = useRef(null);
+  const [currentPosition, setCurrentPosition] = useState(0);
 
   const getFilmURL = () => {
     return data.Film.file_url;
   };
 
-
-  const handleChapterClick = (pos) => {
+  const handleTimeUpdate = (event) => {
     if (playerRef.current) {
-      playerRef.current.seek(parseFloat(pos));
+      setCurrentPosition(Math.floor(event.target.currentTime));
     }
   };
 
   return (
     <div className="video-container">
-      <Player ref={playerRef} playsInline src={getFilmURL()}>
+      <Player ref={playerRef} playsInline src={getFilmURL()} onTimeUpdate={handleTimeUpdate}>
       </Player>
-      <ChapterList onChapterClick={handleChapterClick} />
+      <ChapterList onChapterClick={(pos) => playerRef.current.seek(parseFloat(pos))} />
+      <KeywordList currentPosition={currentPosition} />
     </div>
   );
 };
 
 export default Video;
-
